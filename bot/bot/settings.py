f09974af6283a000e079b4003d678d968aa75df8
koteakhaladze <koteakh@gmail.com>
2021-10-24 23:30:41 +0400

    static files

--- bot/bot/settings.py
+++ bot/bot/settings.py
@@ -119,6 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR.joinpath(BASE_DIR, 'static_root')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = (
+    BASE_DIR.joinpath(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

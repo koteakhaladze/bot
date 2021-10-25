26f8eb87b4afa288e3885812f8ece1fc5f801e7a
koteakhaladze <koteakh@gmail.com>
2021-10-25 10:34:14 +0400

    settings

--- bot/trade/admin.py
+++ bot/trade/admin.py
@@ -4,4 +4,4 @@ from .models import Message
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ('ticket', 'action', 'message', 'created_at')
+    list_display = ('ticket', 'action', 'message', 'result', 'result_at')
--- bot/trade/models.py
+++ bot/trade/models.py
@@ -5,8 +5,9 @@ class Message(models.Model):
     ticket = models.CharField(max_length=50)
     action = models.CharField(max_length=50)
     message = models.CharField(max_length=500)
-    created_at = models.DateTimeField(auto_now_add=True)
+    result = models.CharField(max_length=500, blank=True)
+    result_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         verbose_name_plural = 'Messages'
-        ordering = ['created_at']
+        ordering = ['result_at']
